0fa0397acd744c18bfaf70d1b47d89126fcc4c71
ayudian12 <70583611+ayudian12@users.noreply.github.com>
2020-10-16 13:01:01 +0700

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS A407M
+ */ 
+public class Biodata {
+    public void biodataNama(){
+        System.out.println("Nama : Ayudian Rahmadhani");
+    }
+
+     public void biodataKelas(){
+         System.out.println("Kelas : XI");
+    }
+     public void biodataJurusan(){
+         System.out.println("Jurusan : Rekayasa Persangkat Lunak");
+    }
+  
+     public void biodataAbsen(){
+         System.out.println("Absen : 06");
+    }
+ 
+ public void biodataAlamat(){
+      System.out.println("Alamat :Ds.Parerejo, Kec.Purwodadi, Kab.Pasuruan, JawaTimur, Indonesia");
+    }  
+
+  public void biodataTTL(){
+         System.out.println("TTL : Malang, 12 November 2003");
+    }
+
+    public void biodataUmur(){
+         System.out.println("Umur:  17 Tahun");
+    }
+}
+    
---
+++ Utama.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS A407M
+ */
+public class Utama {
+   public static void main(String [] args){
+       Biodata biodata = new Biodata();
+       biodata.biodataNama();
+       biodata.biodataKelas();
+       biodata.biodataJurusan();
+       biodata.biodataAbsen();
+       biodata.biodataAlamat();
+       biodata.biodataTTL();
+       biodata.biodataUmur();
+ 
+    }
+
+}
